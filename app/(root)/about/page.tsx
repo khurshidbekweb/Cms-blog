fecd6b5463245bde76bde62e28a2ff66384d3abf
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-09-26 00:03:13 +0500

    end home page

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className='bg-red-200'>
+            About Page
+        </div>
+    );
+};
+
+export default AboutPage;
--- types/index.tsx
+++ types/index.tsx
@@ -1,3 +1,12 @@
 export interface ChildProps{
     children: React.ReactNode;
 }
+
+export interface IBlogs{
+    title: string,
+    description: string,
+    author: string,
+    tags: string[],
+    date: string,
+    image: string,
+}
