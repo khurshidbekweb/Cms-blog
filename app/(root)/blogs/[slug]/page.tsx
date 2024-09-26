cddb94cdce1cbca2db71bdc6884b464b3e73e5e3
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-09-26 18:53:23 +0500

    all Ui ended

--- app/(root)/blogs/[slug]/page.tsx
+++ app/(root)/blogs/[slug]/page.tsx
@@ -1,9 +1,13 @@
-import { CalendarDays, Clock, Minus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight, CalendarDays, Clock, Facebook, Link2, Linkedin, Minus, Send, Twitter } from "lucide-react";
 import Image from "next/image";
+import parse from 'html-react-parser'
+import { content } from "@/constants";
+import Link from "next/link";
 
 const SlugPage = () => {
     return (
-        <div className="pt-[15vh] max-w-5xl mx-auto">
+        <div className="pt-[15vh] max-w-5xl mx-auto px-3 md:px-1">
             <h1 className="lg:text-6xl md:text-5xl text-4xl font-creteRound">
                 The AGI hype train is running out of steam
             </h1>
@@ -36,7 +40,57 @@ const SlugPage = () => {
 				height={`595`}
 				className='mt-4 rounded-md'
 			/>
-            <div className="flex "></div>
+            <div className="flex md:gap-12 max-md:flex-col-reverse mt-12 ">
+				<div className="flex flex-col space-y-8">
+					<div className="sticky top-36">
+						<h2 className="text-lg uppercase text-muted-foreground">Share</h2>
+						<div className=" flex flex-col max-md:flex-row md:space-y-3 max-md:space-x-3 mt-4">
+							<Button size={'icon'} variant={'outline'}>
+									<Twitter />
+							</Button>
+							<Button size={'icon'} variant={'outline'}>
+								<Facebook />
+							</Button>
+							<Button size={'icon'} variant={'outline'}>
+								<Linkedin />
+							</Button>
+							<Button size={'icon'} variant={'outline'}>
+								<Send />
+							</Button>
+							<Button size={'icon'} variant={'outline'}>
+								<Link2 />
+							</Button>
+						</div>
+					</div>
+				</div>
+				<div className="flex-1 prose dark:prose-invert">
+					{parse(content)}
+				</div>
+			</div>
+			<div className='flex mt-6 gap-6 items-center max-md:flex-col'>
+				<Image
+					src={'/author/chris-impey.jpg'}
+					alt='author'
+					width='155'
+					height='155'
+					className='rounded-md max-md:self-start mx-auto '
+				/>
+				<div className='flex-1 flex flex-col space-y-4'>
+					<h2 className='text-3xl font-creteRound'>Thomas Macaulay</h2>
+					<p className='line-clamp-2 text-muted-foreground'>
+						Thomas Macaulay is a writer based in New York City. He is interested
+						in all things tech, science, and photography related, and likes to
+						yo-yo in
+					</p>
+					<Link
+						href={'/'}
+						className='flex items-center gap-2 hover:text-blue-500 underline transition-colors'
+					>
+						<span>See all posts by this author</span>
+						<ArrowUpRight />
+					</Link>
+				</div>
+			</div>
         </div>
     );
 };
